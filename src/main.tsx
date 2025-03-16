5a69da850713234a04eb124651f364f422a796c2
Nagmani001 <nagmanipd3@gmail.com>
2025-03-16 08:27:10 +0545

    asdf

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} appearance={{ baseTheme: { showPoweredBy: false } }} afterSignOutUrl="/">
       <App />
     </ClerkProvider>
   </React.StrictMode>,
